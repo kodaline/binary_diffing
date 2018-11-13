2774f0e9610d26e44057fb7ff873b8f46a353f01
kodaline <adelinadoncheva.georgieva@mail.polimi.it>
2018-11-13 14:40:48 +0100

    Timing introduced
    Use of time library in order to calculate the time that the algorithm takes in order to finish a single analysis

--- test.py
+++ test.py
@@ -8,84 +8,8 @@ import matplotlib.pylab as plt
 import helper
 from multiprocessing import Process, Queue, cpu_count
 import dill as pickle
+from time import time
 from operator import itemgetter
-def show_graphic(args):
-    for filename in args:
-        opcode_dictionary = defaultdict(lambda:0)
-        
-        buffer = create_memory_buffer_with_contents_of_file(filename)
-        context = get_global_context()
-        module = context.parse_ir(buffer)
-        for function in module.iter_functions():
-            for bb in function.iter_basic_blocks():
-                for instruction in bb.iter_instructions():
-                    opcode_dictionary[instruction.instruction_opcode] += 1
-                    
-        lists = sorted(opcode_dictionary.items()) # sorted by key, return a list of tuples
-        
-        x, y = zip(*lists) # unpack a list of pairs into two tuples
-        
-        plt.plot(x, y, label=filename)
-        plt.legend(loc='best')
-    plt.show()
-
-
-def show_sim_opcode(args):
-    buffer_1 = create_memory_buffer_with_contents_of_file(args[0])
-    buffer_2 = create_memory_buffer_with_contents_of_file(args[1])
-    context = get_global_context()
-    module_1 = context.parse_ir(buffer_1)
-    module_2 = context.parse_ir(buffer_2)
-    l_bb_1 = [bb for fun_1 in module_1.iter_functions() for bb in fun_1.iter_basic_blocks()]
-    l_bb_2 = [bb for fun_2 in module_2.iter_functions() for bb in fun_2.iter_basic_blocks()]
-    l_bb_1_len = len(l_bb_1)
-    l_bb_2_len = len(l_bb_2)
-    if len(l_bb_1) > len(l_bb_2):
-        first = l_bb_2
-        last = l_bb_1
-    else:
-        first = l_bb_1
-        last = l_bb_2
-
-    one_shot_match = 0
-    drift_match = 0
-    never_matched = 0
-    found = False
-    i = 0
-    while(True):
-        if not found:
-            i += 1
-        if (i >= len(first) or i >= len(last)):
-            break
-        j = i
-        found = False
-        while (j < len(last)):
-            opcode_1 = [inst.instruction_opcode for inst in first[i].iter_instructions()]
-            opcode_2 = [inst.instruction_opcode for inst in last[j].iter_instructions()]
-            if opcode_1 == opcode_2:
-                if i == j:
-                    #print i , "and" , j , "match!"
-                    first.remove(first[i])
-                    last.remove(last[j])
-                    one_shot_match += 1
-                else:
-                    #print "...found with" , j
-                    first.remove(first[i])
-                    last.remove(last[j])
-                    drift_match += 1
-                found = True
-                break
-            else:
-                if i == j:
-                    pass
-                    #print i , "and" , j , "does NOT match! Starting to drift until match..."
-                j += 1
-        if not found:
-            never_matched += 1
-            #print "...match never found"
-    print "#BB for module 1: ", l_bb_1_len, "\n#BB for module 2: ", l_bb_2_len
-    print "Match at first attempt: ", one_shot_match, "\nMatch after some drift: ", drift_match, "\nNever matched: ", never_matched
-    print "Remaining unmatched blocks:\n", first, "\n", last
 
 def flatten(container):
     for i in container:
@@ -96,17 +20,13 @@ def flatten(container):
             yield i
 
 def compute_csv(args):
-    
     # Queue(s) for the process, one for input data to the process, the other for the output data to the main process
-    
     q_in = Queue()
     q_out = Queue()
-     
     try:
         cpus = cpu_count() - 1
     except NotImplementedError:
         cpus = 2   # arbitrary default
-    
     buffer_1 = create_memory_buffer_with_contents_of_file(args[0])
     buffer_2 = create_memory_buffer_with_contents_of_file(args[1])
     context = get_global_context()
@@ -116,24 +36,19 @@ def compute_csv(args):
     list_opcodes = get_opcodes([module_1, module_2])
     global helper_names
     helper_names = get_helper_names(module_1, module_2)
-    
     header = list(flatten(['function1', 'function2', 'match', '#bb_ratio', '#bb_diff', '#instr_ratio', '#instr_diff', 'byte_dim_ratio', 'byte_dim_diff', '#instructions_ratio', '#instructions_diff', 'load_dim_ratio', 'load_dim_diff', '#loads_ratio', '#loads_diff', 'store_dim_ratio', 'store_dim_diff', '#lstores_ratio', '#stores_diff', '#indirect_calls_ratio', '#indirect_calls_diff', '#function_calls_ratio', '#function_calls_diff']))
     header.extend(list(flatten([[str(elem) + "_ratio", str(elem) + "_diff"] for elem in helper_names])))
-    
     header.extend(list(flatten([[str(elem) + "_ratio", str(elem) + "_diff"] for elem in list_opcodes])))
 
     #pool = Pool(cpus) 
-    
     functions_list = [get_names, cmp_name, cmp_dimension_llvm_bb, cmp_dimension_llvm_instr, cmp_byte_dimension_num_instr, cmp_load_store_instructions, cmp_indirect_calls, cmp_revamb_function_calls, cmp_helper_calls, cmp_instruction_opcodes]
-    
-     
     # "enumerate" takes the list and returns a tuple composed by (index_of_element, element)
     [q_in.put((i, pickle.dumps(x))) for i, x in enumerate(functions_list)]
     [q_in.put((-1, -1)) for _ in xrange(cpus)]
 
-    tuples_space = [(fun1, fun2) for fun1 in module_1.iter_functions() for fun2 in module_2.iter_functions() if "bb." in fun1.name and "bb." in fun2.name]    
+    tuples_space = [(fun1, fun2) for fun1 in module_1.iter_functions() for fun2 in module_2.iter_functions() if "bb." in fun1.name and "bb." in fun2.name]
     # Starting the process
-    rows = [] 
+    rows = []
     proc = [Process(target=run, args=(tuples_space, q_in, q_out, i)) for i in xrange(cpus)]
 
     for p in proc:
@@ -146,13 +61,12 @@ def compute_csv(args):
     [p.join() for p in proc]
     rows = [elem[1] for elem in sorted(rows, key=itemgetter(0))]
     rows = [list(flatten(elem)) for elem in zip(*rows)]
-
-
     #rows = [list(flatten(map(lambda x: x(fun1, fun2), functions_list))) for fun1 in module_1.iter_functions() for fun2 in module_2.iter_functions() if "bb." in fun1.name and "bb." in fun2.name]
     '''
     rows = [list(flatten([get_names(fun1, fun2), compare_name(fun1, fun2), cmp_dimension_llvm_bb(fun1, fun2), cmp_dimension_llvm_instr(fun1, fun2), cmp_byte_dimension_num_instr(fun1, fun2), cmp_load_instructions(fun1, fun2), cmp_store_instructions(fun1, fun2), cmp_indirect_calls(fun1, fun2), cmp_revamb_function_calls(fun1, fun2), cmp_num_function_calls(fun1, fun2), cmp_helper_calls(fun1, fun2), cmp_instruction_opcodes(fun1, fun2)])) for fun1 in module_1.iter_functions() for fun2 in module_2.iter_functions() if "bb." in fun1.name and "bb." in fun2.name]
     '''
-    helper.write(rows, header)
+    filename = args[2]
+    helper.write(rows, header, filename=filename)
 
 def run(tuples_space, q_in, q_out, i):
     while True:
@@ -163,7 +77,6 @@ def run(tuples_space, q_in, q_out, i):
             break
         result = map(lambda x: pickle.loads(target)(x[0], x[1]), tuples_space)
         q_out.put((identifier, result))
-        
 
 def get_names(fun1, fun2):
     return [fun1.name, fun2.name]
@@ -181,7 +94,7 @@ def cmp_dimension_llvm_bb(fun1, fun2):
     bb1 = fun1.count_basic_blocks()
     bb2 = fun2.count_basic_blocks()
     return [(bb1 + bb2)/2, bb1-bb2]
-    
+
 def cmp_dimension_llvm_instr(fun1, fun2):
     count1 = 0
     for bb in fun1.iter_basic_blocks():
@@ -203,7 +116,7 @@ def get_opcodes(array_module):
                     opcode_set.add(instruction.instruction_opcode)
     return sorted(list(opcode_set))
 
-def get_opcode_dictionary(function): 
+def get_opcode_dictionary(function):
     opcode_dictionary = defaultdict(lambda:0)
 
     for bb in function.iter_basic_blocks():
@@ -245,17 +158,15 @@ def get_loads_stores(function):
         for instruction in bb.iter_instructions():
             if instruction.is_a_load_inst() != None and instruction.get_operand(0).is_a_global_variable() == None:
                 load_inst += instruction.type_of().get_int_type_width()
-                len_load_inst += 1 
+                len_load_inst += 1
             if instruction.is_a_store_inst() != None and instruction.get_operand(1).is_a_global_variable() == None:
                 store_inst += instruction.type_of().get_int_type_width()
                 len_store_inst += 1
     return load_inst / 8.0, len_load_inst, store_inst / 8.0, len_store_inst
 
 def cmp_load_store_instructions(fun1, fun2):
-    
-    fun1_loads_dim, fun1_loads_count, fun1_store_dim, fun1_store_count = get_loads_stores(fun1) 
+    fun1_loads_dim, fun1_loads_count, fun1_store_dim, fun1_store_count = get_loads_stores(fun1)
     fun2_loads_dim, fun2_loads_count, fun2_store_dim, fun2_store_count = get_loads_stores(fun2)
-    
     return [(fun1_loads_dim + fun2_loads_dim)/2, fun1_loads_dim - fun2_loads_dim, (fun1_loads_count + fun2_loads_count)/2, fun1_loads_count - fun2_loads_count, (fun1_store_dim + fun2_store_dim)/2, fun1_store_dim - fun2_store_dim, (fun1_store_count + fun2_store_count)/2, fun1_store_count - fun2_store_count]
 
 def get_helper_names(module1, module2):
@@ -270,9 +181,7 @@ def get_helper_names(module1, module2):
     return set(helper_names)
 
 def get_helper_calls(function, helper_set):
-    
-    helper_names = [instruction.get_operand(instruction.get_num_operands() - 1).get_name() for bb in function.iter_basic_blocks() for instruction in bb.iter_instructions() if instruction.is_a_call_inst() != None]    
-    
+    helper_names = [instruction.get_operand(instruction.get_num_operands() - 1).get_name() for bb in function.iter_basic_blocks() for instruction in bb.iter_instructions() if instruction.is_a_call_inst() != None]
     return Counter(helper_names)
 
 def get_revamb_function_calls(function):
@@ -295,15 +204,12 @@ def cmp_revamb_function_calls(fun1, fun2):
     return [(fun1_revamb_calls + fun2_revamb_calls)/2, fun1_revamb_calls - fun2_revamb_calls]
 
 
-def cmp_helper_calls(fun1, fun2): 
-               
+def cmp_helper_calls(fun1, fun2):
     fun1_helper_calls = get_helper_calls(fun1, helper_names)
     fun2_helper_calls = get_helper_calls(fun2, helper_names)
-    
     return [[(fun1_helper_calls[elem] + fun2_helper_calls[elem])/2, fun1_helper_calls[elem] - fun2_helper_calls[elem]] for elem in helper_names]
 
 def get_indirect_calls(function):
-    
     dispatcher_calls = 0
     for bb in function.iter_basic_blocks():
         for instruction in bb.iter_instructions():
@@ -313,7 +219,6 @@ def get_indirect_calls(function):
     return dispatcher_calls
 
 def cmp_indirect_calls(fun1, fun2):
-    
     fun1_indirect_calls = get_indirect_calls(fun1)
     fun2_indirect_calls = get_indirect_calls(fun2)
 
@@ -325,11 +230,10 @@ def get_num_function_calls(function):
         for instruction in bb.iter_instructions():
             if instruction.is_a_call_inst() != None:
                 if "function_call" in instruction.print_value_to_string():
-                    count += 1    
-    return count 
+                    count += 1
+    return count
 
 def cmp_num_function_calls(fun1, fun2):
-    
     fun1_function_calls = get_num_function_calls(fun1)
     fun2_function_calls = get_num_function_calls(fun2)
 
@@ -337,14 +241,19 @@ def cmp_num_function_calls(fun1, fun2):
 
 
 
-function_map = { 
-    'graphic': show_graphic,
-    'opcodes': show_sim_opcode,
+function_map = {
+    #'graphic': show_graphic,
+    #'opcodes': show_sim_opcode,
     'csv': compute_csv
 }
 parser = argparse.ArgumentParser()
 parser.add_argument('command')
-parser.add_argument('filename', nargs=2)
+parser.add_argument('filename', nargs=3)
 args= parser.parse_args()
 function = function_map[args.command]
+start_time = time()
 function(args.filename)
+end_time = time()
+total_min = (end_time - start_time)/60 
+total_sec = (end_time - start_time) - total_min * 60
+print "\n Total time:", total_min, "min", total_sec, "sec" 
